807f85aef24dcf9321a433d348f6a2960f885456
Jonas Ferreira <jonasferreirasil@hotmail.com>
2025-05-13 17:21:18 -0300

    added chancejs

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -1,10 +1,13 @@
 import userData from '../fixtures/user-Data.json'
-import myInfo from '../fixtures/my-Info.json'
 import LoginPage from '../pages/loginPage.js'
 import dashboard from '../pages/dashboard.js'
 import MenuPage from '../pages/menuPage.js'
 import MyInfoPage from '../pages/myInfoPage.js'
 
+const Chance = require('chance');
+
+
+const chance = new Chance()
 const loginPage = new LoginPage
 const dash = new dashboard
 const menuPage = new MenuPage
@@ -14,26 +17,23 @@ const myInfo = new MyInfoPage
 
 describe('Orange HRM Test', () => {
 
-  const selectorList = {
-
-
-  }
-
-
   it('Login Success', () => {
     loginPage.accessLoginPage()
     loginPage.loginWithUsers(userData.userSucess.username, userData.userSucess.password)
     dash.checkDashboardAcess()
     menuPage.accessMyInfo()
-    myInfo.filledField()
+    myInfo.filledPersonalField(chance.first(),chance.string(),chance.last())
+    myInfo.filledEmployField(chance.ssn({ dashes: false }),chance.ssn({ dashes: false }),chance.ssn({ dashes: false }),'2023-05-01')
+    myInfo.filledEmploy2Field('2001-05-07')
+    myInfo.filledStatus()
+
    })
    
  
 
   })
-  // it('Login - Fail', () => {
-  //   cy.visit('/auth/login')
-  //   cy.get(selectorList.usernameField).type(userData.userFail.username)
-  //   cy.get(selectorList.passwordField).type(userData.userFail.password)
-  //   cy.get(selectorList.loginButton).click()
-  //   cy.get(selectorList.wrongCredentialAlert)
+  it('Login - Fail', () => {
+    loginPage.accessLoginPage()
+    loginPage.loginWithUsers(userData.userFail.username,userData.userFail.password)
+    loginPage.invalidAcess()
+  })
--- cypress/pages/myInfoPage.js
+++ cypress/pages/myInfoPage.js
@@ -19,24 +19,33 @@ class MyInfoPage {
         }
 
         
-    filledField(){
-            cy.get(this.selectorList.firstNameField).clear().type("Primeiro")
-            cy.get(this.selectorList.middleNameField).clear().type("Meio")
-            cy.get(this.selectorList.lastNameField).clear().type("Ultimo")
-            cy.get(this.selectorList.genericField).eq(4).clear().type('Employee')
-            cy.get(this.selectorList.genericField).eq(5).clear().type('Other Id')
-            cy.get(this.selectorList.genericField).eq(6).clear().type('Driver')
-            cy.get(this.selectorList.dateField).eq(0).clear().type('2020-05-01')
-            cy.get(this.selectorList.dateCloseButton).click()
-            cy.get(this.selectorList.genericTwoField).eq(0).click()
-            cy.get(this.selectorList.brazilianOpction).click()
-            cy.get(this.selectorList.genericTwoField).eq(1).click()
-            cy.get(this.selectorList.martialStatusOpction).click()
-            cy.get(this.selectorList.dateField).eq(1).clear().type('2001-10-08')
-            cy.get(this.selectorList.dateCloseButton).click()
-            cy.get(this.selectorList.maleSelectedOpction).click()
-            cy.get(this.selectorList.buttonSaveSubmit).eq(0).click({force: true})
-            cy.get('body').should('contain', 'Successfully Updated')
+    filledPersonalField(first,middle,last){
+            cy.get(this.selectorList().firstNameField).clear().type(first)
+            cy.get(this.selectorList().middleNameField).clear().type(middle)
+            cy.get(this.selectorList().lastNameField).clear().type(last)
+           
+    }
+    filledEmployField(employeeID,otherID,driver,dateLicense){
+        cy.get(this.selectorList().genericField).eq(4).clear().type(employeeID)
+        cy.get(this.selectorList().genericField).eq(5).clear().type(otherID)
+        cy.get(this.selectorList().genericField).eq(6).clear().type(driver)
+        cy.get(this.selectorList().dateField).eq(0).clear().type(dateLicense)
+        cy.get(this.selectorList().dateCloseButton).click()
+    }
+
+    filledEmploy2Field(datebirth){
+        cy.get(this.selectorList().genericTwoField).eq(0).click()
+        cy.get(this.selectorList().brazilianOpction).click()
+        cy.get(this.selectorList().genericTwoField).eq(1).click()
+        cy.get(this.selectorList().martialStatusOpction).click()
+        cy.get(this.selectorList().dateField).eq(1).clear().type(datebirth)
+        cy.get(this.selectorList().dateCloseButton).click()
+        cy.get(this.selectorList().maleSelectedOpction).click()
+    }
+
+    filledStatus(){
+        cy.get(this.selectorList().buttonSaveSubmit).eq(0).click({force: true})
+        cy.get('body').should('contain', 'Successfully Updated')
     }
     
     
